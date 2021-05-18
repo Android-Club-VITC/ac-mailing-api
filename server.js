486e1614264cdbc75d52c6bd977660b869c3455e
Naman <namanddev7@gmail.com>
2021-05-19 01:17:09 +0530

    remved console.logs

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(cors())
 
 app.post('/api/sendmail',async (req,res)=>{
 	try{
-		console.log(req.body);
 		const response = await mailHandler(req.body);
 		if(response){
 			res.send({status: response});
