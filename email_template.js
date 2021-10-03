2c1e4ef70d491995543afa401d7f81a848958320
Naman <naman.agarwal2019@vitstudent.ac.in>
2021-10-03 20:42:39 +0530

    add git-commit template

--- email_template.js
+++ email_template.js
@@ -10,7 +10,29 @@ const data = {
                 cid: "logo"
             }
         ]
-    }       
+    },
+    
+    "git-commit": {
+        path: path.join(__dirname,"templates","git_commit.html"),
+        attachments: [
+            {
+                filename: "logo.png",
+                path: path.join(__dirname,"assets","logo.png"),
+                cid: "logo"
+            }
+        ]
+    }
 }
+
+// {
+//     filename: "git_logo.png",
+//     path: path.join(__dirname,"assets","git_logo.png"),
+//     cid: "git_logo"
+// },
+// {
+//     filename: "git_bg.png",
+//     path: path.join(__dirname,"assets","git_bg.png"),
+//     cid: "git_bg"
+// }
 
 module.exports = data;
